e95ac4f04682a8a251d76825ef03c885254a3157
BillyBernard <billybernard54@gmail.com>
2021-09-06 20:49:00 -0600

    fixed the nasty bugs

--- src/components/Squares.js
+++ src/components/Squares.js
@@ -60,7 +60,8 @@ export default function Squares() {
           // Nasty bug! We should map over a slice of state, instead of 'listOfSquareIds'.
           // We might say: "it works, though!" But if the list of squares is not state,
           // we could never add squares, change squares or remove squares in the future. Fix!
-          listOfSquareIds.map(id =>
+          squares.map(id =>
+            //listOfSquareIds.map
             <div
               id={id}
               key={id}
